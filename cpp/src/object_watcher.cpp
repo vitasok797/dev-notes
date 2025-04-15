af89d232f8c5499950861653884f2d73f773015d
vitasok797 <vitasok@gmail.com>
2025-04-15 16:42:38 +0300

    Create object_watcher.cpp

---
+++ cpp/src/object_watcher.cpp
@@ -0,0 +1,49 @@
+#include <atomic>
+#include <iostream>
+
+class ObjWatcher final
+{
+public:
+    ObjWatcher() noexcept : index_(++counter_)
+    {
+        std::cout << "ObjWatcher: created (" << index_ << ")" << std::endl;
+    };
+
+    ObjWatcher(const ObjWatcher& other) noexcept : index_(++counter_)
+    {
+        std::cout << "ObjWatcher: created (" << index_ << ") copy from (" << other.index_ << ")" << std::endl;
+    };
+
+    ObjWatcher(ObjWatcher&& other) noexcept : index_(++counter_)
+    {
+        other.moved_ = true;
+        std::cout << "ObjWatcher: created (" << index_ << ") move from (" << other.index_ << ")" << std::endl;
+    };
+
+    ObjWatcher& operator=(const ObjWatcher& other) noexcept
+    {
+        moved_ = false;
+        std::cout << "ObjWatcher: (" << index_ << ") copy assigned from (" << other.index_ << ")" << std::endl;
+        return *this;
+    };
+
+    ObjWatcher& operator=(ObjWatcher&& other) noexcept
+    {
+        moved_ = false;
+        other.moved_ = true;
+        std::cout << "ObjWatcher: (" << index_ << ") move assigned from (" << other.index_ << ")" << std::endl;
+        return *this;
+    };
+
+    ~ObjWatcher() noexcept
+    {
+        std::cout << "ObjWatcher: destroyed (" << index_ << ")";
+        if (moved_) std::cout << " [moved]";
+        std::cout << std::endl;
+    };
+
+private:
+    static inline std::atomic<size_t> counter_ = 0;
+    size_t index_;
+    bool moved_ = false;
+};
