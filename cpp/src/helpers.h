31aeec1d0c26b6315159d1bfe6f9ade8c6423530
vitasok797 <vitasok@gmail.com>
2025-04-17 17:18:04 +0300

    Create helpers.h

---
+++ cpp/src/helpers.h
@@ -0,0 +1,14 @@
+#ifndef VS_UTIL_HELPERS_H_
+#define VS_UTIL_HELPERS_H_
+
+#include <type_traits>
+
+namespace vs::util
+{
+
+template<class T> auto as_signed  (T t){ return std::make_signed_t<T>(t); }
+template<class T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
+
+}  // namespace vs::util
+
+#endif  // VS_UTIL_HELPERS_H_
