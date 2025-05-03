bfd813d06943fd39bfde2e79bf04e5aedd064c4f
vitasok797 <vitasok@gmail.com>
2025-05-03 18:55:22 +0300

    Update calc.h

--- cpp/src/calc.h
+++ cpp/src/calc.h
@@ -1,9 +1,21 @@
 #ifndef VS_CALC_H_
 #define VS_CALC_H_
 
+#include <algorithm>
+#include <cmath>
+
 namespace vs
 {
 
+inline bool almost_equal(double a, double b, double abs_epsilon)
+{
+    return std::abs(a - b) <= abs_epsilon;
+}
+
+inline bool almost_equal_rel(double a, double b, double rel_epsilon = 1e-6)
+{
+	return std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * rel_epsilon);
+}
 
 }  // namespace vs
 
