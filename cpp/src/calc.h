c3451816dcd16868e4d80a07c33fda694d588888
vitasok797 <vitasok@gmail.com>
2025-05-03 18:59:50 +0300

    Update calc.h

--- cpp/src/calc.h
+++ cpp/src/calc.h
@@ -13,7 +13,7 @@ inline bool almost_equal(double a, double b, double abs_epsilon)
 
 inline bool almost_equal_rel(double a, double b, double rel_epsilon = 1e-6)
 {
-	return std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * rel_epsilon);
+    return std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * rel_epsilon);
 }
 
 }  // namespace vs
