40a7166abb84f76b8909ef2fc77e6000b2604411
vitasok797 <vitasok@gmail.com>
2025-05-04 16:09:48 +0300

    Update util.h

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -1,19 +1,26 @@
 #ifndef VS_UTIL_UTIL_H_
 #define VS_UTIL_UTIL_H_
 
+#include <cstddef>
 #include <functional>
 #include <optional>
 #include <type_traits>
 
-namespace vs::util
+namespace vs
+{
+
+using Index = std::ptrdiff_t;
+
+template<typename T> auto as_signed(T t){ return std::make_signed_t<T>(t); }
+template<typename T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
+
+namespace util
 {
 
 template<typename T>
 using optional_ref = std::optional<std::reference_wrapper<T>>;
 
-template<typename T> auto as_signed(T t){ return std::make_signed_t<T>(t); }
-template<typename T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
-
-}  // namespace vs::util
+}  // namespace util
+}  // namespace vs
 
 #endif  // VS_UTIL_UTIL_H_
