64fe4acfc60e26631be9e8edcc55d911329fffdc
vitasok797 <vitasok@gmail.com>
2025-05-05 16:15:27 +0300

    Update util.h

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -9,17 +9,17 @@
 namespace vs
 {
 
+template<typename T> auto as_signed(T t){ return std::make_signed_t<T>(t); }
+template<typename T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
+
 using Index = std::ptrdiff_t;
 using SignedSize = std::ptrdiff_t;
 
 template<typename T>
 constexpr SignedSize signed_size(const T& x)
 {
-    return vs::as_signed(x.size());
+    return as_signed(x.size());
 }
-
-template<typename T> auto as_signed(T t){ return std::make_signed_t<T>(t); }
-template<typename T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
 
 namespace util
 {
