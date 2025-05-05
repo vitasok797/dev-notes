2d6085ffe3d6b1fd042251edafa1cffcb2bf4501
vitasok797 <vitasok@gmail.com>
2025-05-05 15:52:07 +0300

    Update util.h

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -10,6 +10,7 @@ namespace vs
 {
 
 using Index = std::ptrdiff_t;
+using SignedSize = std::ptrdiff_t;
 
 template<typename T> auto as_signed(T t){ return std::make_signed_t<T>(t); }
 template<typename T> auto as_unsigned(T t){ return std::make_unsigned_t<T>(t); }
