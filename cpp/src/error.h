76c2cf229750de9e50f6ad92a503d3e738666b5c
vitasok797 <vitasok@gmail.com>
2025-05-20 21:36:22 +0300

    Update error.h

--- cpp/src/error.h
+++ cpp/src/error.h
@@ -11,6 +11,26 @@ struct nullptr_error : public std::exception
     auto what() const noexcept -> const char* override { return "nullptr_error"; }
 };
 
+template<typename T>
+auto ptr_check(const T& ptr) -> void
+{
+    if (!ptr) throw nullptr_error();
+}
+
+template<typename T>
+auto ptr_checked_deref(const T& ptr) -> auto&
+{
+    ptr_check(ptr);
+    return *ptr;
+}
+
+template<typename T>
+auto ptr_checked_get(const T& ptr) -> auto
+{
+    ptr_check(ptr);
+    return ptr.get();
+}
+
 }  // namespace vs
 
 #endif  // VS_ERROR_H_
