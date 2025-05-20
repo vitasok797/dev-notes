d5f953d50a3498a58ede4f2d711750d46dd1343b
vitasok797 <vitasok@gmail.com>
2025-05-20 20:49:53 +0300

    Update error.h

--- cpp/src/error.h
+++ cpp/src/error.h
@@ -1,9 +1,15 @@
 #ifndef VS_ERROR_H_
 #define VS_ERROR_H_
 
+#include <exception>
+
 namespace vs
 {
 
+struct nullptr_error : public std::exception
+{
+    auto what() const noexcept -> const char* override { return "nullptr_error"; }
+};
 
 }  // namespace vs
 
