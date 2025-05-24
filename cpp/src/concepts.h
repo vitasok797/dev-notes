7454a209a6100f64437dd13745ca325af00651e2
vitasok797 <vitasok@gmail.com>
2025-05-24 20:49:11 +0300

    Update concepts.h

--- cpp/src/concepts.h
+++ cpp/src/concepts.h
@@ -13,6 +13,9 @@ concept callable = std::is_invocable_v<Fn, Args...>;
 template<typename Ret, typename Fn, typename... Args>
 concept callable_r = std::is_invocable_r_v<Ret, Fn, Args...>;
 
+template<typename T, typename... Types>
+concept one_of_types = (std::same_as<std::decay_t<T>, Types> || ...);
+
 template<typename T, typename U>
 concept same_type_as = std::same_as<std::decay_t<T>, U>;
 
