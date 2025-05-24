993d5bcaccd6aa0266d787bf1443dc22466d2f17
vitasok797 <vitasok@gmail.com>
2025-05-24 22:27:56 +0300

    Update util.h

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -5,6 +5,7 @@
 #include <functional>
 #include <optional>
 #include <type_traits>
+#include <utility>
 
 namespace vs
 {
@@ -23,6 +24,26 @@ constexpr auto signed_size(const T& t) -> SignedSize
 
 template<typename T>
 using optional_ref = std::optional<std::reference_wrapper<T>>;
+
+template<typename T, typename U>
+constexpr auto&& forward_like(U&& x) noexcept
+{
+    constexpr bool is_adding_const = std::is_const_v<std::remove_reference_t<T>>;
+    if constexpr (std::is_lvalue_reference_v<T&&>)
+    {
+        if constexpr (is_adding_const)
+            return std::as_const(x);
+        else
+            return static_cast<U&>(x);
+    }
+    else
+    {
+        if constexpr (is_adding_const)
+            return std::move(std::as_const(x));
+        else
+            return std::move(x);
+    }
+}
 
 }  // namespace vs
 
