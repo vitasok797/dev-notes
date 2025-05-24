942f1485d530d771b09dd6255756a6f99a9d1beb
vitasok797 <vitasok@gmail.com>
2025-05-24 19:06:21 +0300

    Update util.h

--- cpp/src/util.h
+++ cpp/src/util.h
@@ -1,7 +1,6 @@
 #ifndef VS_UTIL_H_
 #define VS_UTIL_H_
 
-#include <concepts>
 #include <cstddef>
 #include <functional>
 #include <optional>
@@ -24,9 +23,6 @@ constexpr auto signed_size(const T& t) -> SignedSize
 
 template<typename T>
 using optional_ref = std::optional<std::reference_wrapper<T>>;
-
-template<typename T1, typename T2>
-concept same_type_as = std::same_as<std::decay_t<T1>, T2>;
 
 }  // namespace vs
 
