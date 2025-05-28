380d233d8652dec437dc41efa65acacdbfe381f4
vitasok797 <vitasok@gmail.com>
2025-05-28 20:26:52 +0300

    Update thread.h

--- cpp/src/thread.h
+++ cpp/src/thread.h
@@ -1,8 +1,18 @@
 #ifndef VS_THREAD_H_
 #define VS_THREAD_H_
 
+#include <future>
+#include <utility>
+
 namespace vs
 {
+
+template<typename F, typename... Param>
+inline auto run_async(F&& f, Param&&... param)
+{
+    return std::async(std::launch::async, std::forward<F>(f), std::forward<Param>(param)...);
+}
+
 }  // namespace vs
 
 #endif  // VS_THREAD_H_
