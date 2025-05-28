0e40103dda7a9db57488764d5464c7f82903f06e
vitasok797 <vitasok@gmail.com>
2025-05-28 22:46:44 +0300

    Update thread.h

--- cpp/src/thread.h
+++ cpp/src/thread.h
@@ -2,6 +2,9 @@
 #define VS_THREAD_H_
 
 #include <future>
+#include <mutex>
+#include <thread>
+#include <unordered_map>
 #include <utility>
 
 namespace vs
@@ -11,6 +14,22 @@ template<typename F, typename... Param>
 inline auto run_async(F&& f, Param&&... param)
 {
     return std::async(std::launch::async, std::forward<F>(f), std::forward<Param>(param)...);
+}
+
+inline auto get_cur_thread_num() -> int
+{
+    static auto mutex = std::mutex{};
+    auto scope_lock = std::scoped_lock(mutex);
+
+    static auto next_num = 1;
+    static auto thread_nums = std::unordered_map<std::thread::id, int>{};
+
+    auto cur_id = std::this_thread::get_id();
+    auto [item, inserted] = thread_nums.insert({cur_id, next_num});
+
+    if (inserted) ++next_num;
+
+    return item->second;
 }
 
 }  // namespace vs
