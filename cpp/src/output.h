0957ccf1f2b7243e8ea939717e4e99c1adc30085
vitasok797 <vitasok@gmail.com>
2025-05-29 12:45:01 +0300

    Update output.h

--- cpp/src/output.h
+++ cpp/src/output.h
@@ -1,8 +1,28 @@
 #ifndef VS_OUTPUT_H_
 #define VS_OUTPUT_H_
 
+#include <iostream>
+
 namespace vs
 {
+
+template<typename... Values>
+auto println(std::ostream& os, const Values&... values) -> void
+{
+    if constexpr (sizeof...(values) > 0)
+    {
+        auto n = 0;
+        ((os << (n++ == 0 ? "" : " ") << values), ...);
+    }
+    os << std::endl;
+}
+
+template<typename... Values>
+auto println(const Values&... values) -> void
+{
+    println(std::cout, values...);
+}
+
 }  // namespace vs
 
 #endif  // VS_OUTPUT_H_
