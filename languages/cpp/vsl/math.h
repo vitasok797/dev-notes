b160549eff2544a56677f490c97206d304f5840b
vitasok797 <vitasok@gmail.com>
2025-06-08 23:21:20 +0300

    Update math.h

--- languages/cpp/vsl/math.h
+++ languages/cpp/vsl/math.h
@@ -20,6 +20,22 @@ inline auto almost_equal_rel(double a, double b, double rel_epsilon = 1e-6) -> b
     return std::abs(a - b) <= (std::fmax(std::abs(a), std::abs(b)) * rel_epsilon);
 }
 
+using TrimBounds = std::pair<std::optional<double>, std::optional<double>>;
+inline constexpr auto no_trim = std::nullopt;
+
+inline auto ftrim(double value, TrimBounds bounds) -> double
+{
+    auto lo = bounds.first.value_or(double_max_neg);
+    auto hi = bounds.second.value_or(double_max);
+
+    if (lo > hi)
+    {
+        throw std::logic_error{"ftrim bounds error (low > high)"};
+    }
+
+    return std::clamp(value, lo, hi);
+}
+
 }  // namespace vsl
 
 #endif  // VSL_MATH_H_
