dde7220b37779ce01a93b437f6bff7fcbd7def43
vitasok797 <vitasok@gmail.com>
2025-06-09 00:04:32 +0300

    Update math.h

--- languages/cpp/vsl/math.h
+++ languages/cpp/vsl/math.h
@@ -4,9 +4,10 @@
 #include <algorithm>
 #include <cmath>
 #include <limits>
-#include <optional>
 #include <stdexcept>
 #include <utility>
+
+#define INF HUGE_VAL
 
 namespace vsl
 {
@@ -24,13 +25,10 @@ inline auto almost_equal_rel(double a, double b, double rel_epsilon = 1e-6) -> b
     return std::abs(a - b) <= (std::fmax(std::abs(a), std::abs(b)) * rel_epsilon);
 }
 
-using TrimBounds = std::pair<std::optional<double>, std::optional<double>>;
-inline constexpr auto no_trim = std::nullopt;
-
-inline auto ftrim(double value, TrimBounds bounds) -> double
+inline auto ftrim(double value, std::pair<double, double> bounds) -> double
 {
-    auto lo = bounds.first.value_or(double_max_neg);
-    auto hi = bounds.second.value_or(double_max);
+    auto lo = bounds.first;
+    auto hi = bounds.second;
 
     if (lo > hi)
     {
